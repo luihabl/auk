1c872ce52fb479963cf7ee30235224e281b5ddac
Lui Habl <txaicalvoso@gmail.com>
2021-03-28 09:25:35 +0200

    Refactoring opengl debug messages

--- src/graphics/graphics.cpp
+++ src/graphics/graphics.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <glad/glad.h>
+#include <string>
 
 #include "tinysdl/graphics/graphics.h"
 #include "tinysdl/platform/log.h"
@@ -18,54 +19,54 @@ void Graphics::load_gl_functions() {
 
 void Graphics::setup_gl_debug() {
     int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
-    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
-    {
+    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
         Log::debug("OpenGL debug activated");
         glEnable(GL_DEBUG_OUTPUT);
-        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); // makes sure errors are displayed synchronously
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
         glDebugMessageCallback(Graphics::gl_debug_callback, nullptr);
         glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
     }
 }
 
 void APIENTRY Graphics::gl_debug_callback(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char *message, const void *user_param) {
-    // ignore non-significant error/warning codes
+
     if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return; 
-
-    std::cout << "---------------" << std::endl;
-    std::cout << "Debug message (" << id << "): " <<  message << std::endl;
-
-    switch (source)
-    {
-        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
-        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
-        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
-        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
-        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
-        case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
-    } std::cout << std::endl;
-
-    switch (type)
-    {
-        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
-        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
-        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break; 
-        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
-        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
-        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
-        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
-        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
-        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
-    } std::cout << std::endl;
     
-    switch (severity)
-    {
-        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
-        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
-        case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
-        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
-    } std::cout << std::endl;
-    std::cout << std::endl;
+    std::string source_msg;
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:              source_msg = "API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:    source_msg = "Window system"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:  source_msg = "Shader compiler"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:      source_msg = "Third party"; break;
+        case GL_DEBUG_SOURCE_APPLICATION:      source_msg = "Application"; break;
+        case GL_DEBUG_SOURCE_OTHER:            source_msg = "Other"; break;
+        default:                               source_msg = "-";
+    } 
+
+    std::string type_msg;
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:                type_msg = "Error"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:  type_msg = "Deprecated behaviour"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:   type_msg = "Undefined behaviour"; break; 
+        case GL_DEBUG_TYPE_PORTABILITY:          type_msg = "Portability"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:          type_msg = "Performance"; break;
+        case GL_DEBUG_TYPE_MARKER:               type_msg = "Marker"; break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:           type_msg = "Push group"; break;
+        case GL_DEBUG_TYPE_POP_GROUP:            type_msg = "Pop group"; break;
+        case GL_DEBUG_TYPE_OTHER:                type_msg = "Other"; break;
+        default:                                 type_msg = "-";
+    }
+    
+    std::string severity_msg;
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:         severity_msg = "High"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:       severity_msg = "Medium"; break;
+        case GL_DEBUG_SEVERITY_LOW:          severity_msg = "Low"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION: severity_msg = "Notification"; break;
+        default:                             severity_msg = "-";
+    } 
+
+    Log::debug("OpenGL message [%i]: %s\nsource: %s\ntype: %s\nseverity: %s\n", id, message, source_msg.c_str(), type_msg.c_str(), severity_msg.c_str());
 }
 
 void Graphics::clear(const Color & c) {
