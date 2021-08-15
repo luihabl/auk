050657a3ec74ef3e095e863a1490bb34a0e7e818
Lui Habl <txaicalvoso@gmail.com>
2021-08-15 14:02:08 -0300

    Adding Type class

--- include/tinysdl/platform/type_id.h
+++ include/tinysdl/platform/type_id.h
@@ -2,17 +2,27 @@
 
 namespace TinySDL
 {
-    class TypeID {
-        
+
+    struct Type {
+
+    private:
+        unsigned int id;
         static inline unsigned int type_count = 0;
-    
-    public:
+        
+        Type(unsigned int i) { id = i; }
+
+    public: 
+        Type() = default;
+        
+        bool operator== (const Type & other){return id == other.id;}
+        bool operator!= (const Type & other){return id != other.id;}
+
         static unsigned int count() {return type_count;}
         
         template<typename T>
-        static unsigned int get() {
-            static const unsigned int id = type_count++;
-            return id;    
+        static Type type_of() {
+            static const unsigned int _id = type_count++;
+            return Type(_id);    
         }
     };
 }
