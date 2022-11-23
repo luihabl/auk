73a504a8fb5b77e091620c605966ca82ae95eb84
Lui Habl <txaicalvoso@gmail.com>
2022-11-23 21:47:05 +0100

    Adding type hash function

--- include/auk/platform/type_id.h
+++ include/auk/platform/type_id.h
@@ -1,29 +1,45 @@
 #pragma once
 
+#include <functional>
+
 namespace auk
 {
-
-    struct Type {
+    struct Type
+    {
 
     private:
         size_t id;
         static inline size_t type_count = 0;
-        
+
         Type(size_t i) { id = i; }
 
-    public: 
+    public:
         Type() = default;
-        
-        bool operator== (const Type & other){return id == other.id;}
-        bool operator!= (const Type & other){return id != other.id;}
-        operator size_t() const {return id;}
 
-        static size_t count() {return type_count;}
-        
-        template<typename T>
-        static Type type_of() {
+        bool operator==(const Type &other) { return id == other.id; }
+        bool operator!=(const Type &other) { return id != other.id; }
+        operator size_t() const { return id; }
+
+        static size_t count() { return type_count; }
+
+        template <typename T>
+        static Type type_of()
+        {
             static const size_t _id = type_count++;
-            return Type(_id);    
+            return Type(_id);
         }
     };
+}
+
+namespace std
+{
+    template <>
+    struct hash<auk::Type>
+    {
+        std::size_t operator()(const auk::Type &t) const
+        {
+             return t;
+        }
+    };
+
 }
